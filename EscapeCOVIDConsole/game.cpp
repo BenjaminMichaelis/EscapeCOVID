c3f27a2d8342af3b399dfc9845417fa785c1cf9b
Unknown <jaredite.cam@gmail.com>
2020-06-17 14:28:06 -0700

    Full Game Loop #WIP
    Game over audio now plays
    Press ENTER to restart game (doesn't reset viruses or score yet)

--- EscapeCOVIDConsole/game.cpp
+++ EscapeCOVIDConsole/game.cpp
@@ -11,74 +11,80 @@ void gameInstance() {
         Vector2f playerPos, virusPos;
         bool collision = false;
         // run the game while window stays open:
-        while (Window.window.isOpen() && collision == false)
+        while (Window.window.isOpen())
         {
-            if (VirusCreationTracker == FirstVirus)
+            while (collision == false)
             {
-                Window.virus01.CreateVirusOBJ(Window.player);
-            }
-            if (VirusCreationTracker == SecondVirus)
-            {
-                Window.virus02.CreateVirusOBJ(Window.player);
-            }
-            if (VirusCreationTracker == ThirdVirus)
-            {
-                Window.virus03.CreateVirusOBJ(Window.player);
-            }
-            if (VirusCreationTracker == FourthVirus)
-            {
-                Window.virus04.CreateVirusOBJ(Window.player);
-            }
-            if (VirusCreationTracker == FifthVirus)
-            {
-                Window.virus05.CreateVirusOBJ(Window.player);
-            }
-            //Window.player.getSprite().getPosition().x;
-            //Window.virus01.getSprite().getPosition().x;
+                if (VirusCreationTracker == FirstVirus)
+                {
+                    Window.virus01.CreateVirusOBJ(Window.player);
+                }
+                if (VirusCreationTracker == SecondVirus)
+                {
+                    Window.virus02.CreateVirusOBJ(Window.player);
+                }
+                if (VirusCreationTracker == ThirdVirus)
+                {
+                    Window.virus03.CreateVirusOBJ(Window.player);
+                }
+                if (VirusCreationTracker == FourthVirus)
+                {
+                    Window.virus04.CreateVirusOBJ(Window.player);
+                }
+                if (VirusCreationTracker == FifthVirus)
+                {
+                    Window.virus05.CreateVirusOBJ(Window.player);
+                }
 
-            collision = Window.checkCollision(VirusCreationTracker);
+                collision = Window.checkCollision(VirusCreationTracker);
 
+                // main game loop, i.e. all movements and interactions go below this line:
+                // player movement controls:
+                if (Keyboard::isKeyPressed(Keyboard::Left)) {
+                    Window.player.moveLeft();
+                }
+                if (Keyboard::isKeyPressed(Keyboard::Right)) {
+                    Window.player.moveRight();
+                }
+                if (Keyboard::isKeyPressed(Keyboard::Up)) {
+                    Window.player.moveUp();
+                }
+                if (Keyboard::isKeyPressed(Keyboard::Down)) {
+                    Window.player.moveDown();
+                }
+
+                // virus auto-movement:
+                if (VirusCreationTracker >= FirstVirus)
+                {
+                    Window.virus01.autoMovement();
+                }
+                if (VirusCreationTracker >= SecondVirus)
+                {
+                    Window.virus02.autoMovement();
+                }
+                if (VirusCreationTracker >= ThirdVirus)
+                {
+                    Window.virus03.autoMovement();
+                }
+                if (VirusCreationTracker >= FourthVirus)
+                {
+                    Window.virus04.autoMovement();
+                }
+                if (VirusCreationTracker >= FifthVirus)
+                {
+                    Window.virus05.autoMovement();
+                }
+
+
+                Window.displayMovements(VirusCreationTracker);
+                VirusCreationTracker++;
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Enter))
+            {
+                collision = false;
+                Graphics Window;
+            }
             Window.closeWindow();
-            // main game loop, i.e. all movements and interactions go below this line:
-            // player movement controls:
-            if (Keyboard::isKeyPressed(Keyboard::Left)) {
-                Window.player.moveLeft();
-            }
-            if (Keyboard::isKeyPressed(Keyboard::Right)) {
-                Window.player.moveRight();
-            }
-            if (Keyboard::isKeyPressed(Keyboard::Up)) {
-                Window.player.moveUp();
-            }
-            if (Keyboard::isKeyPressed(Keyboard::Down)) {
-                Window.player.moveDown();
-            }
-
-            // virus auto-movement:
-            if (VirusCreationTracker >= FirstVirus)
-            {
-                Window.virus01.autoMovement();
-            }
-            if (VirusCreationTracker >= SecondVirus)
-            {
-                Window.virus02.autoMovement();
-            }
-            if (VirusCreationTracker >= ThirdVirus)
-            {
-                Window.virus03.autoMovement();
-            }
-            if (VirusCreationTracker >= FourthVirus)
-            {
-                Window.virus04.autoMovement();
-            }
-            if (VirusCreationTracker >= FifthVirus)
-            {
-                Window.virus05.autoMovement();
-            }
-         
-
-            Window.displayMovements(VirusCreationTracker);
-            VirusCreationTracker++;
-        }
+        }   
 }
 
