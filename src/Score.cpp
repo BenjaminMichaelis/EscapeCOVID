6edd9a1b962a88ff70f43fa4de365f0315eac5f2
bmichael <bmichael@eecs.wsu.edu>
2020-12-05 16:30:35 -0800

    score is fixed to seconds for linux building

--- src/Score.cpp
+++ src/Score.cpp
@@ -2,19 +2,17 @@
 
 Score::Score()
 {
-    score = clock();//grab current time as basis for score
+	score = clock(); //grab current time as basis for score
 }
 
-int Score::updateScore()//every time this function is called, update score
+int Score::updateScore() //every time this function is called, update score
 {
-    int timer = (clock() - score) / 1000;//score is equal to current time minus starting time divided by 1000 to give seconds
+	int timer = (clock() - score) / 100000; //score is equal to current time minus starting time divided by 1000 to give seconds
 
-    return timer;//return time
+	return timer; //return time
 }
 
 void Score::resetScore()
 {
-    score = clock();
-
-    
+	score = clock();
 }
